d8df4f34aec1cb2985c85760c03e1cc7111aca01
Wuriyanto <wuriyanto48@yahoo.co.id>
2019-10-16 00:12:47 +0700

    feat: add basic loop input

--- src/Main.cc
+++ src/Main.cc
@@ -5,17 +5,25 @@
 
 int main(int argc, char* argv[])
 {
-    types::ListString list;
-    int res = argparser::ParseArg(argc, argv, &list);
+    types::ListString args;
+    int res = argparser::ParseArg(argc, argv, &args);
     if (res < 0) 
     {
         std::cout << "required 2 arguments" << std::endl;
         return -1;
     }
 
-    for (std::string arg : list)
+    for (std::string arg : args)
     {
         std::cout << " " << arg << std::endl;
+    }
+
+    std::string input;
+
+    while (input != "exit")
+    {
+        std::cin >> input;
+        std::cout << input << std::endl;
     }
 
     return 0;
